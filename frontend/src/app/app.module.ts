2ec55bdd40af495109c51f2ad7098635350997a3
RutiRozenberg <ruti7633@gmail.com>
2024-05-07 02:11:24 +0300

    Adding a logo to the website

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { JobComponent } from './components/job/job.component';
 import { JobsListComponent } from './components/jobs-list/jobs-list.component';
 import { MainComponent } from './components/main/main.component';
+import { FilterJobsComponent } from './components/filter-jobs/filter-jobs.component';
+import { ShowDetailsComponent } from './components/show-details/show-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +20,17 @@ import { MainComponent } from './components/main/main.component';
     JobComponent,
     JobsListComponent,
     MainComponent,
+    FilterJobsComponent,
+    ShowDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
