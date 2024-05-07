0e8f864cf677bd95868913db4097b1d7b9792e0e
RutiRozenberg <ruti7633@gmail.com>
2024-05-08 02:54:08 +0300

    A user does not have to log in several times until he is recognized and he goes to the jobs page

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,34 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user-service'
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  constructor(private userSrv:UserService,private router:Router, private active:ActivatedRoute){
-
+export class LoginComponent{
+  constructor(private userSvc: UserService, private router: Router, private active: ActivatedRoute) {
+    localStorage.removeItem("currentUser")
   }
-  userData:any ={email: '' , password: ''}
-  isOnClick:boolean=false
 
-   
-  loginClick(){
-    this.isOnClick=true;
-    this.userSrv.getFromServer(this.userData.password , this.userData.email).subscribe((res: any) => this.userSrv.updateUser(res))   
-    if(localStorage.getItem("currentUser")){
-      this.router.navigate(['/' ], {relativeTo:this.active})
+  userData: any = { email: '', password: '' }
+  isOnClick: boolean = false
+  passwordLength: number = 0
+  containSpaces:boolean =false
+
+  loginClick() {
+    this.isOnClick = true
+    this.passwordLength = this.userData.password.length
+    this.containSpaces =  ! (this.userData.password.indexOf(' ') === -1)
+    if (this.userData.email != '' && this.userData.password != '' && this.passwordLength >= 8 && ! this.containSpaces) {
+      this.userSvc.getFromServer(this.userData.password, this.userData.email).subscribe((res: any) => {
+        if (res) {
+          localStorage.setItem('currentUser', res)
+          this.userSvc.updateUser(res)
+          this.router.navigate(['/'], { relativeTo: this.active })
+        }
+        else {
+          if (this.userData.email != '' || this.userData.password != '') {
+            this.userData.email = ''
+            this.userData.password = ''
+            alert('User does not exist. Try again')
+            this.isOnClick = false
+          }
+        }
+      })
     }
-    else{
-      if(this.userData.email!='' && this.userData.password!=''){
-        this.userData.email=''
-        this.userData.password=''
-        alert('User does not exist. Try again')
-        this.isOnClick=false
-      }  
-    } 
   }
-
 }
